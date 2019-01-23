834d10059a1fdc4e1cd7bd81e2ec31254ad804da
liaohengfan <liaohengfan@yeah.net>
2019-01-23 09:37:15 +0800

    vscode C C++ project init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void shellInsertSort(int a[], int n, int dp){
+
+}
+int main(int argc, char *args[]){
+    cout <<"hello world!"<<endl;
+    getchar();
+    return 0;
+}
